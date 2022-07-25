d1ea5aba2de7b74b785058ffea24f1ece56ae75e
and-roque <amlroque@gmail.com>
2022-07-25 11:35:47 +0100

    Create arithmetic functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function add (a,b) {return a+b;}
+
+function subtract (a,b) {return a-b;}
+
+function multiply (a,b) {return a*b;}
+
+function divide (a,b) {return a/b;}
+
+let a = 5;
+let b = 2;
+
+console.log(add(a,b));
+console.log(subtract(a,b));
+console.log(multiply(a,b));
+console.log(divide(a,b));
