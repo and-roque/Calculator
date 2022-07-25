5208f691d37debaccbfdfcafc430aebf5557701a
and-roque <amlroque@gmail.com>
2022-07-25 11:45:25 +0100

    Create operation function

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
+//Arithmetic functions
 function add (a,b) {return a+b;}
-
 function subtract (a,b) {return a-b;}
-
 function multiply (a,b) {return a*b;}
-
 function divide (a,b) {return a/b;}
 
+//TEST
 let a = 5;
 let b = 2;
+let operator = "*";
+console.log(operate (a,b,operator));
 
-console.log(add(a,b));
+/* console.log(add(a,b));
 console.log(subtract(a,b));
 console.log(multiply(a,b));
-console.log(divide(a,b));
+console.log(divide(a,b)); */
+
+//Operate
+function operate (a,b,operator) {
+    switch (operator) {
+        case "+":
+            return add(a,b);
+            /* break; */
+        case "-":
+            return subtract(a,b);
+            break;
+        case "*":
+            return multiply(a,b);
+            break;
+        case "/":
+            return divide(a,b);
+            break;
+    }
+};
