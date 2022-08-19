0571c67580f4390ead4dfd9d621c5b22ed0aafbd
and-roque <amlroque@gmail.com>
2022-08-19 11:08:35 +0100

    Associate functions to buttons

--- script.js
+++ script.js
@@ -1,34 +1,115 @@
+//Divs
+const display = document.querySelector('.display');
+const lastValue = document.querySelector('.lastValue');  
+const buttons = document.querySelectorAll("button");
+
+//Common Values
+let displayValue="";
+let currentValue="";
+let currentOperator="";
+let waitingValue="";
+let dotCheck = false;
+let opCheck = false;
+let eqCheck = true;
+const inputValue = document.querySelectorAll('.number');
+const inputOperator = document.querySelectorAll('.operator');
+const startOperation = document.querySelectorAll('.equal');
+const clearOperation = document.querySelectorAll('.clear');
+const deleteValue = document.querySelectorAll('.delete');
+
+
 //Arithmetic functions
-function add (a,b) {return a+b;}
+function add (a,b) {return Number(a)+Number(b);}
 function subtract (a,b) {return a-b;}
 function multiply (a,b) {return a*b;}
 function divide (a,b) {return a/b;}
-
-//TEST
-let a = 5;
-let b = 2;
-let operator = "*";
-console.log(operate (a,b,operator));
-
-/* console.log(add(a,b));
-console.log(subtract(a,b));
-console.log(multiply(a,b));
-console.log(divide(a,b)); */
 
 //Operate
 function operate (a,b,operator) {
     switch (operator) {
         case "+":
             return add(a,b);
-            /* break; */
         case "-":
             return subtract(a,b);
-            break;
-        case "*":
+        case "x":
             return multiply(a,b);
-            break;
         case "/":
             return divide(a,b);
-            break;
     }
 };
+
+function populateDisplay (input){
+    displayValue = displayValue + input;
+    display.textContent = displayValue;
+    lastValue.textContent=`${waitingValue} ${currentOperator}`;
+}
+
+ //Event Listeners
+
+ //Numbers
+ inputValue.forEach((button) => {
+    button.addEventListener('click', () => {
+        currentValue = button.textContent;
+    
+        //Make sure "." doesn't get it more then once
+        if (currentValue==="." && dotCheck === false && displayValue===""){
+           currentValue = "0.";
+           dotCheck = true; 
+        } else if(dotCheck === true && currentValue===".") {
+            currentValue = "";
+        } else if(currentValue===".") dotCheck = true;
+
+        populateDisplay(currentValue);
+    })
+ });
+
+ //Operators
+inputOperator.forEach((button) => {
+    button.addEventListener('click', ()=> {
+        currentOperator = button.textContent;
+        display.textContent = `${displayValue} ${currentOperator}`;
+        if (opCheck === true){
+            waitingValue = operate(waitingValue,displayValue,currentOperator);
+        }
+        waitingValue = displayValue;
+        displayValue = "";
+        dotCheck = false; 
+        opCheck = true;
+        eqCheck = true;
+        
+    })
+});
+
+//Equal
+startOperation.forEach((button) => {
+    button.addEventListener('click', ()=> {
+        Number(displayValue);
+        Number(waitingValue);
+        
+        if(eqCheck===true && waitingValue != ""){
+          lastValue.textContent=`${waitingValue} ${currentOperator} ${displayValue} = `;
+          displayValue = operate(waitingValue,displayValue,currentOperator);
+          eqCheck = false; 
+        }
+        display.textContent = displayValue;
+        
+    })
+});
+
+//Clear
+clearOperation.forEach((button) => {
+    button.addEventListener('click', ()=> {
+        displayValue = "";
+        waitingValue = "";
+        display.textContent = displayValue;
+        lastValue.textContent = waitingValue;
+    })
+});
+
+//Delete Value
+deleteValue.forEach((button) => {
+    button.addEventListener('click', ()=> {
+        displayValue = "";
+        display.textContent = displayValue;
+    })
+});
